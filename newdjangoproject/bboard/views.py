172a6766858ae4c8c5341e327974aafa77a41e78
AnsarAshirbekov <ans962011@gmail.com>
2025-11-25 22:03:43 +0500

    Материал занятия 03 наполовину

--- newdjangoproject/bboard/views.py
+++ newdjangoproject/bboard/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from .models import Bb
+from .models import Bb, Rubric
 
 def index(request):
-    # s = "Список объявлений\r\n\r\n\r\n"
-    # for bb in Bb.objects.order_by('-published'):
-    #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    # return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    bbs = Bb.objects.order_by("-published")
+    return render(request, 'bboard/index.html', context)
 
-    # template = loader.get_template('bboard/index.html')
-    # bbs = Bb.objects.order_by('-published')
-    # context = {'bbs': bbs}
-    # return HttpResponse(template.render(context, request))
-
-    bbs = Bb.objects.order_by('-published')
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
