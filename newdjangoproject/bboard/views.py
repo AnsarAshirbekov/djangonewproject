0f520a5880b2fc2485eb70fd3e02d30ec636a12f
AnsarAshirbekov <ans962011@gmail.com>
2025-11-25 19:46:18 +0500

    Материал занятия 02

--- newdjangoproject/bboard/views.py
+++ newdjangoproject/bboard/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Bb
 
 def index(request):
-    return HttpResponse("Здесь будет список объявлений.")
+    # s = "Список объявлений\r\n\r\n\r\n"
+    # for bb in Bb.objects.order_by('-published'):
+    #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    # return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
+    # template = loader.get_template('bboard/index.html')
+    # bbs = Bb.objects.order_by('-published')
+    # context = {'bbs': bbs}
+    # return HttpResponse(template.render(context, request))
+
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
